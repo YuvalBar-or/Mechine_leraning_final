e172fe6129c5507a0adba5b3344f3ad6da002952
YuvalBar-or <yuvalb.mail@gmail.com>
2024-07-29 21:55:02 +0300

    Update Decision_Tree.py

--- Decision_Tree.py
+++ Decision_Tree.py
@@ -69,7 +69,6 @@ def apply_decision_tree_and_print_accuracy(main_folder_path, csv_file_path):
     print(f"Accuracy: {accuracy:.2f}")
 
 
-# Specify the path to your main folder and CSV file here
 main_folder_path = 'dataset'
 csv_file_path = 'data.csv'
 
